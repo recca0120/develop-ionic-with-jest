32d569e125725935301734a500eba58795c2aaf4
recca0120 <recca0120@gmail.com>
2018-04-23 18:47:40 +0800

    toBeCalledWith

--- src/providers/push/push.spec.ts
+++ src/providers/push/push.spec.ts
@@ -32,8 +32,8 @@ describe('推播測試', () => {
         const pushObject: PushObject = new PushObject(options);
         const pushProvider: PushProvider = new PushProvider(push, http, alertCtrl);
 
-        const pushSpy: jasmine.Spy = spyOn(push, 'init').and.returnValue(pushObject);
-        const pushObjectSpy: jasmine.Spy = spyOn(pushObject, 'on').and.returnValues(
+        spyOn(push, 'init').and.returnValue(pushObject);
+        spyOn(pushObject, 'on').and.returnValues(
             {
                 subscribe: (cb: Function) => {
                     cb({
@@ -51,55 +51,46 @@ describe('推播測試', () => {
                 },
             }
         );
-        const httpSpy: jasmine.Spy = spyOn(http, 'post').and.callThrough();
-        const alertCtrlCreateSpy: jasmine.Spy = spyOn(alertCtrl, 'create').and.returnValue(alertCtrl);
-        const alertCtrlPresentSpy: jasmine.Spy = spyOn(alertCtrl, 'present').and.returnValue(alertCtrl);
+
+        spyOn(http, 'post').and.callThrough();
+        spyOn(alertCtrl, 'create').and.returnValue(alertCtrl);
+        spyOn(alertCtrl, 'present').and.returnValue(alertCtrl);
 
         pushProvider.setup();
 
-        expect(httpSpy.calls.argsFor(0)).toEqual([
-            'http://app.dev/',
-            {
-                registrationId: 'foo',
-                registrationType: 'APN',
-            },
-        ]);
+        expect(http.post).toBeCalledWith('http://app.dev/', {
+            registrationId: 'foo',
+            registrationType: 'APN',
+        });
 
-        expect(alertCtrlCreateSpy.calls.argsFor(0)).toEqual([
-            {
-                buttons: ['Dismiss'],
-                subTitle: '訊息內容',
-                title: '訊息標題',
-            },
-        ]);
+        expect(alertCtrl.create).toBeCalledWith({
+            buttons: ['Dismiss'],
+            subTitle: '訊息內容',
+            title: '訊息標題',
+        });
     });
 
     it('推播測試 fake', done => {
         const push: Push = new FakePush() as any;
         const pushProvider: PushProvider = new PushProvider(push, http, alertCtrl);
 
-        const httpSpy: jasmine.Spy = spyOn(http, 'post').and.callThrough();
-        const alertCtrlCreateSpy: jasmine.Spy = spyOn(alertCtrl, 'create').and.returnValue(alertCtrl);
-        const alertCtrlPresentSpy: jasmine.Spy = spyOn(alertCtrl, 'present').and.returnValue(alertCtrl);
+        spyOn(http, 'post').and.callThrough();
+        spyOn(alertCtrl, 'create').and.returnValue(alertCtrl);
+        spyOn(alertCtrl, 'present').and.returnValue(alertCtrl);
 
         pushProvider.setup();
 
-        expect(httpSpy.calls.argsFor(0)).toEqual([
-            'http://app.dev/',
-            {
-                registrationId: 'foo',
-                registrationType: 'APN',
-            },
-        ]);
+        expect(http.post).toBeCalledWith('http://app.dev/', {
+            registrationId: 'foo',
+            registrationType: 'APN',
+        });
 
         setTimeout(() => {
-            expect(alertCtrlCreateSpy.calls.argsFor(0)).toEqual([
-                {
-                    buttons: ['Dismiss'],
-                    subTitle: '訊息內容',
-                    title: '訊息標題',
-                },
-            ]);
+            expect(alertCtrl.create).toBeCalledWith({
+                buttons: ['Dismiss'],
+                subTitle: '訊息內容',
+                title: '訊息標題',
+            });
             done();
         }, 3000);
     });
